9e4d3e1a6a8687e9e2d1c0d4f707e8cedc4c4a69
Luckykholi <kholidipesh2@gmail.com>
2025-03-11 08:40:54 +0000

    Index file

---
+++ index/script.js
@@ -0,0 +1,260 @@
+
+
+// // // // // // let Binary = "1101";
+// // // // // // let Desimal = 0;
+
+// // // // // // let BinaryLength = Binary.length;
+// // // // // // for( let i = 0; i < BinaryLength; i++) {
+// // // // // //     let Digital = parseInt(Binary[i]);
+// // // // // //     let Power = BinaryLength - i - 1;
+// // // // // //     Desimal += Digital* Math.pow(2, Power);
+// // // // // // }
+
+// // // // // // console.log(Desimal)
+
+// // // // // //Third Method on Binarry to deciamal system;
+
+// // // // // // let Binary = "1101";
+
+// // // // // // let Result = Binary.split('').reverse().reduce( function(acc, digit,index) {
+// // // // // //     return acc + parseInt(digit) * Math.pow(2 , index)
+// // // // // // }, 0);
+
+// // // // // // console.log(Result);
+
+// // // // // let Binary = "1010";
+
+// // // // // let Result = Binary.split('').reverse().reduce(function(acc,digit, index) {
+// // // // //     return acc + parseInt(digit) * Math.pow(2, index);
+// // // // // }, 0)
+
+// // // // // console.log(Result)
+
+// // // // // let Digtial = parseInt(Binary , 2)
+// // // // // console.log(Digtial);
+
+
+
+// // // // // let code = 10;
+// // // // // let final = code.toString(2);
+
+// // // // // console.log(final);
+
+// // // // // let Num = 10;
+// // // // // let Emp = '';
+
+// // // // // while(Num > 0 ) {
+// // // // //     Emp = (Num % 2) + Emp;
+// // // // //     Num = Math.floor(Num / 2)
+// // // // // }
+
+// // // // // console.log(Emp)
+
+// // // // let Binary = "1101";
+// // // // let secont = parseInt(Binary , 2)
+// // // // console.log(secont)
+// // // // let Result = Binary.split('').reverse().reduce( function(acc,digit,index) {
+// // // //     return acc + parseInt(digit) * Math.pow(2 , index)
+// // // // }, 0)
+// // // // console.log(Result);
+// // // // let desimalSystme = 0;
+// // // // let BianryNumber = Binary.length;
+
+// // // // for (let i = 0; i < BianryNumber; i++) {
+// // // //     let Digit = parseInt(Binary[i]);
+// // // //     let Power = BianryNumber - i - 1;
+// // // //     desimalSystme += Digit* Math.pow(2 , Power);
+// // // // }
+
+// // // // console.log(desimalSystme);
+
+// // // // let Remove = 13;
+// // // // let Comp =  '';
+
+// // // // while(Remove > 0 ) {
+// // // //     Comp = (Remove % 2) + Comp;
+// // // //     Remove = Math.floor(Remove / 2)
+// // // // }
+// // // // console.log(Comp)
+// // // // let Remove1 = 13
+// // // // let Empty = Remove1.toString(2);
+// // // // console.log(Empty)
+
+
+
+// // // let Binary1 = "1101";
+// // // let Code = parseInt(Binary1 , 2);
+// // // console.log(Code);
+
+// // // let Binary = "1101";
+// // // let Decimal = 0;
+// // // let BinaryCode = Binary.length;
+
+// // // for( let i = 0; i < BinaryCode; i++) {
+// // //     D = parseInt(Binary[i]);
+// // //     P = BinaryCode - i - 1;
+// // //     Decimal += D * Math.pow(2 , p)
+// // // }
+
+// // // console.log(Decimal);
+
+// // // for(let i = 0; i < 10; i++) {
+// // //     console.log("hii i am lucky i am form dehradun")
+// // // }
+
+// // // let Myname =["BMW", "      Volvo", "   Saab", "    Ford", "     Fiat", "      Audi"];
+// // // let Digital = "";
+
+// // // for(let i = 0; i < Myname.length; i++) {
+// // //     Digital += Myname[i] 
+// // // }
+
+// // // console.log(Digital);
+
+// // // let Name = [
+// // //     Username = "Dipesh                  ",
+// // //     age = 123 ,
+// // //     email = "               kholidipesh2@gmail.com"
+// // // ]
+
+// // // let Roll = '';
+
+// // // for(let i = 0; i < Name.length; i++) {
+// // //     Roll += Name[i];
+// // // }
+
+// // // console.log(Roll);
+
+
+// // //javascript for loop
+// // // for(let i = 0 ; i < 10; i++) {
+// // //     console.log("hii i am dipesh kholi")
+// // // }
+
+// // // //javascript for loop in
+
+// // // let obj = {
+// // //     Name : " Dipesh Kholi",
+// // //     Age : " 23",
+// // //     Email : " kholidipesh2@gmail.com",
+// // //     Last_Name : " Kholi"
+// // // }
+
+// // // for(let b in obj ) {
+// // //     console.log(b  +obj[b])
+// // // }
+
+// // // let Hellow = {
+// // //     Name : " Dipesh Kholi",
+// // //     Age : " 23",
+// // //     Email : " kholidipesh2@gmail.com",
+// // //     Last_Name : " Kholi",
+// // //     Name : " Dipesh Kholi",
+// // //     Age : " 23",
+// // //     Email : " kholidipesh2@gmail.com",
+// // //     Last_Name : " Kholi",
+// // //     Name : " Dipesh Kholi",
+// // //     Age : " 23",
+// // //     Email : " kholidipesh2@gmail.com",
+// // //     Last_Name : " Kholi",
+// // //     Name : " Dipesh Kholi",
+// // //     Age : " 23",
+// // //     Email : " kholidipesh2@gmail.com",
+// // //     Last_Name : " Kholi",
+// // //     Name : " Dipesh Kholi",
+// // //     Age : " 23",
+// // //     Email : " kholidipesh2@gmail.com",
+// // //     Last_Name : " Kholi"
+// // // }
+
+// // // for(let X in Hellow) {
+// // //   console.log(X + Hellow[X])
+// // // }
+
+// // //let to the beinginers in the collage of the..
+
+// // let  Report = {
+// //     name : " Dipesh kholi"
+// // }
+
+// // for(let Y in Report) {
+// //   alert(Y + Report[Y])
+// // console.log(Y + Report[Y])
+// // }
+
+// // for(let i = 0 ; i < 10; i ++ ) {
+// //     debugger
+// //     console.log("Hellow i am lucky kholi")
+// // }
+
+
+// // let Apple = ["Mango", "Apple", "March", "India" ]
+
+// // for(How of Apple) {
+// //     console.log(How)
+// // }
+
+// //while  loops
+
+// // let i = 1;
+
+// // while(i < 10) {
+// //     console.log(i);
+// //     i++;
+// // }
+
+// let Binary = "1101";
+// let Decimal = parseInt(Binary , 2);
+// console.log(Decimal);
+
+// let B = "1101";
+// let D = 0;
+
+// let BinaryLength = B.length;
+
+// let Bol  = "1101";
+// let Dot = '';
+
+// let Result = Bol.split('').reverse().reduce(function(acc , digit, index) {
+//     return acc + parseInt(digit) * Math.pow( 2, index);
+// }, 0)
+
+// console.log(Result);
+
+
+// for(let i = 0; i < BinaryLength; i++) {
+//      Digit = parseInt(B[i]);
+//      Power = BinaryLength - i - 1;
+//      D += Digit * Math.pow(2 , Power);
+// }
+
+// console.log(D);
+// let Des = 13;
+// let Bin = Des.toString(2);
+// console.log(Bin)
+
+
+
+
+// console.log(UserLoggedIn("Dipesh")
+
+// function callback() {
+//    let Binarry = "1101";
+//    let Decimal = parseInt(Binarry , 2)
+   
+//    return console.log(Decimal)
+
+// //    let Binary = "1101";
+// //    let Decimal = 0;
+
+// //    let BinaryLength = Binary.length;
+
+// //    for(let i = 0; i < BinaryLength; i++) {
+// //       Digt = parseInt(Binary[i]);
+// //       Power = BinaryLength - i - 1;
+// //       Decimal += Digt * Math.pow(Power , 2)
+// //    }
+// //    console.log(Decimal);
+// }
+
+// callback()
---
+++ index/scriptjs.js
@@ -0,0 +1,147 @@
+
+
+// // // //let for using loop for in
+
+
+// // // // let Bin = "1"
+// // // // while( i < 0) {
+// // // //         console.log("hii i am lucky kholi")
+// // // // }
+
+// // // let Key1 = Symbol("hii i am lucky")
+
+// // // let obj = {
+// // //         Name : "lucky",
+// // //         LastName: "kholi",
+// // //         "FatherName" : "InderSing",
+// // //         age: 13,
+// // //         email : 'kholidipesh2@gmail.com',
+// // //         isLoggedIn: false,
+// // //         isloogedinbyDay: ["sunday", "Monday", "Tuesday"],
+// // //         [Key1] : "MyKey1"
+// // // }
+
+// // // // console.log(obj ["FatherName"])
+// // // // console.log(obj.Key1)
+
+// // // // obj.email = "kholiluckychatgpt.com"
+// // // // Object.freeze(obj)
+// // // obj.email = "LukcyMicrosoft.com2@gmai.com"
+
+// // // obj.greeting = function() {
+// // //         console.log("hii i am lucky kholi");
+// // // }
+// // // console.log(obj.greeting())
+
+// // // obj.Ln36col13 = function() {
+// // //         console.log("hellow i am debugger it is the fresher")
+// // // }
+
+// // // console.log(obj.Ln36col13())
+
+// // // // console.log(greeting())
+
+
+
+// // // console.log(obj.email)
+
+
+
+// // // // console.log(obj["isloogedinbyDay"]);
+// // // // console.log(obj.email)
+
+// // let Key1 = Symbol("hellow guys")
+// // let Score = Symbol("komal")
+
+// // let obj = {
+// //                 Name : "lucky",
+// //                 LastName: "kholi",
+// //                 "FatherName" : "InderSing",
+// //                 age: 13,
+// //                 email : 'kholidipesh2@gmail.com',
+// //                 isLoggedIn: false,
+// //                 isloogedinbyDay: ["sunday", "Monday", "Tuesday"],
+// //                 [Key1] : "MyKey1",
+// //                 [Score] : "Myeky2"
+// // }
+
+// // // console.log(obj)
+
+// // obj.handle = function() {
+// //         console.log(`HIi i am ${this.Name} , and my Iddentity ${this.email}`);
+
+// //         obj.handle2 = function() {
+// //                 console.log("How to how to how ")
+// //         }
+// //         obj.Kom = function() {
+// //                 console.log("hellow console")
+// //         }
+// // }
+
+// // // obj.handle2 = function() {
+// // //         console.log("How to how to how ")
+// // // }
+
+// // console.log(obj.handle())
+// // console.log(obj.handle2())
+
+
+// // let Object = {};
+// // Object.id = "kholidiepsh";
+// // Object.Email = "Luckychatgpt@gmai.com"
+// // Object.isloggedIn = "Monday";
+
+// // console.log(Object)
+
+// let obj = {
+//     1: "a",
+//     2: "B",
+//     2 : "c",
+//     5 : "d"
+// }
+
+
+// let obj2 = {
+//                 Name : "lucky",
+//                 LastName: "kholi",
+//                 "FatherName" : "InderSing",
+//                 age: 13,
+//                 email : 'kholidipesh2@gmail.com',
+//                 isLoggedIn: false,
+//                 isloogedinbyDay: ["sunday", "Monday", "Tuesday"],
+//                 // [Key1] : "MyKey1",
+//                 // [Score] : "Myeky2"
+// }
+
+
+
+// // let obj3 = {obj  , obj2};
+// // console.log(obj3)
+
+// let obj3 = {...obj, ...obj2}
+// console.log(obj3)
+
+// // let obj4 = Object.assign(obj , obj2)
+// // console.log(obj4)
+
+
+// const Cource = {
+//     name: "lucky kholi",
+//     Age : 13, 
+//     Email : "kholidipesh2@gmail.com"
+// }
+
+// // Cource.Email
+
+// const {Email : Rahul}  = Cource
+// console.log(Rahul)
+
+// let my = [1,2,3,4,5,6,7,8]
+
+// my.push(10);
+// my.pop()
+// console.log(my)
+
+let Score = 13.234;
+
+console.log(Score.toPrecision(4))
